b2b1d3915c85995ec38df8064d6af9d34fe13300
Tiago Alves <alvesjtiago@gmail.com>
2017-01-12 21:42:45 +0100

    Initial working version.

---
+++ README.md
@@ -0,0 +1,30 @@
+# Hover Locales
+
+A Sublime Text 3 plugin to show different rails locale strings simply by hovering on a localized string.
+
+## Installation
+
+_macOS_
+```
+cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+git clone --depth=1 https://github.com/alvesjtiago/hover-locales.git
+```
+
+_Ubuntu_
+```
+cd ~/.config/sublime-text-3/Packages
+git clone --depth=1 https://github.com/alvesjtiago/hover-locales.git
+```
+
+_Windows_
+```
+cd "%APPDATA%\Sublime Text 3\Packages"
+git clone --depth=1 https://github.com/alvesjtiago/hover-locales.git
+```
+
+Or manually create a folder named "hover-locales" on your Packages folder and copy the content of this repo to it.
+
+## Contribute
+
+Hover Locales is a small utility created by [Tiago Alves](https://twitter.com/alvesjtiago).
+Any help on this project is more than welcome. Or if you find any problems, please comment or open an issue with as much information as you can provide.
---
+++ hover_locales.py
@@ -0,0 +1,115 @@
+import sublime
+import sublime_plugin
+import struct
+import base64
+import imghdr
+import os
+import re
+from . import yaml
+
+class HoverLocales(sublime_plugin.EventListener):
+    def on_hover(self, view, point, hover_zone):
+        if (hover_zone == sublime.HOVER_TEXT):
+            hovered_line_text = view.substr(view.line(point)).strip()
+
+            next_double_quote = view.find('"', point).a
+            next_single_quote = view.find("'", point).a
+            next_parentheses = view.find(r"\)", point).a
+
+            symbols_dict = { next_double_quote: '"', 
+                             next_single_quote: "'",
+                             next_parentheses: ')' }
+
+            symbols = []
+            if next_double_quote != -1:
+                symbols.append(next_double_quote)
+            if next_single_quote != -1:
+                symbols.append(next_single_quote)
+            if next_parentheses != -1:
+                symbols.append(next_parentheses)
+
+            # Check if symbols exist from the mouse pointer forward
+            if len(symbols) == 0:
+                return
+
+            closest_symbol = min(symbols)
+            symbol = symbols_dict[closest_symbol]
+
+            quote_type = symbol
+
+            regex = r""
+            if symbol == "'":
+                regex = r"'([^']*)'"
+            elif symbol == '"':
+                regex = r'"([^"]*)"'
+            elif symbol == ")":
+                regex = r'\(([^()]*)\)'
+
+            path = re.findall(regex, hovered_line_text)
+
+            # All quotes in view
+            if symbol == ")":
+                all_quotes = view.find_all(r"\(|\)")
+                all_match = [item for item in all_quotes if (item.a == closest_symbol)]
+            else:
+                all_quotes = view.find_all(quote_type)
+                all_match = [item for item in all_quotes if item.a == closest_symbol]
+
+            # If there are no matches return
+            if len(all_match) == 0:
+                return
+
+            # Get final and initial region of quoted string
+            final_region = all_match[0]
+            index = all_quotes.index(final_region) - 1
+            initial_region = all_quotes[index]
+
+            # String path for file
+            path = view.substr(sublime.Region(initial_region.b, final_region.a))
+            path = path.strip().split('/')[-1]
+
+            # Get base project folder
+            base_folder = sublime.active_window().folders()[0] + "/config/locales"
+
+
+            if (path and path != "" and os.path.isdir(base_folder)):
+
+                # Check each locale file for a match
+                for root, dirs, files in os.walk(base_folder):
+                    values = {}
+                    for file in files:
+                        if file.endswith("yml"):
+                            file_name = os.path.join(root, file)
+                            file_split = file.split('.')
+                            locale = file_split[len(file_split) - 2]
+                            path_split = path.split('.')
+
+                            file_yaml = yaml.load(open(file_name, "rb").read())[locale]
+                            for path_section in path_split:
+                                try:
+                                    file_yaml = file_yaml[path_section]
+                                except:
+                                    break
+                            if type(file_yaml) is dict:
+                                continue
+                            else:
+                                values[locale] = file_yaml
+                    if len(values) == 0:
+                        break
+                    html = ""
+                    should_break = False
+                    for key, value in values.items():
+                        if should_break:
+                            html += "<br>"
+                        should_break = True
+
+                        html += "<div>"
+                        html += '<span style="color: #f00;">' + key + "</span>" + ": " + value
+                        html += "</div>"
+                    view.show_popup('<div>' + html + '</div>', 
+                                         flags=sublime.HIDE_ON_MOUSE_MOVE_AWAY, 
+                                         location=point)
+                    break
+            return
+        return
+        
